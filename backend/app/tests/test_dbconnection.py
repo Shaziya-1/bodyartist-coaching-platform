c694988207159e682ed174adf31e3733d609f4f5
Navnath Kadam <navnathkadam284@gmail.com>
2026-06-09 17:03:23 +0530

    chore: initialize the postgres db

---
+++ backend/app/tests/test_dbconnection.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+
+from backend.app.config.database import engine, Base
+from backend.app.models.users_model import User
+
+load_dotenv()
+
+def test_database_connection():
+    print("[INFO] Connecting to local Postgres and creating tables...")
+    try:
+        # 1. Yeh command automatic database mein 'users' table generate kar degi
+        Base.metadata.create_all(bind=engine)
+        print("[SUCCESS] Table 'users' successfully created in your database!")
+
+    except Exception as e:
+        print("[ERROR] Error connecting to database or inserting data:")
+        print(str(e))
+
+if __name__ == "__main__":
+    test_database_connection()
